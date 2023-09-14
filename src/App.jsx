8a5209e19cce1420b70df5226068090a8238f17a
Rintia Islam <rintiaislam110@gmail.com>
2023-09-14 20:49:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='font-bold text-4xl text-center my-8'>Course Registration</h1>
+    </>
+  )
+}
+
+export default App
