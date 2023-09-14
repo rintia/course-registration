bccb5acea5d878f8e57b466e683596dc5c6dded2
Rintia Islam <rintiaislam110@gmail.com>
2023-09-14 20:59:48 +0600

    json data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 
+import { useState } from 'react'
 import './App.css'
 import Courses from './Components/Courses/Courses'
+import { useEffect } from 'react';
 
 function App() {
-  
+   const[allCourses, setAllCourses] = useState([]);
+   useEffect(() => {
+    fetch('./data.json')
+    .then(res => res.json())
+    .then(data => console.log(data))
+   },[])
 
   return (
     <>
